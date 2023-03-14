79b5b39c0bfef8b0ff81d043dc09d6cdb4e41c6f
pdst-lccs <50370310+pdst-lccs@users.noreply.github.com>
2023-03-14 13:03:52 +0000

    Add files via upload

---
+++ halfAdder.py
@@ -0,0 +1,19 @@
+#Irene Stone
+#March 2023
+#Half-Adder Python
+
+#defining a function called halfAdder
+def halfAdder(x,y):  #function takes in two parameters
+    if x!=y:    #checks if x is not equal to y
+        s = 1   #s assigned value of 1 if x is not equal to y
+    else:
+        s = 0   #if x equals y then s is assigned the value 0
+    c = x & y     #& is AND operation 
+    print("Inputs", x, "and", y, ": \t Sum=", s,"\t Carry=", c)
+
+halfAdder(0,0)
+halfAdder(0,1)
+halfAdder(1,0)
+halfAdder(1,1)
+
+    
